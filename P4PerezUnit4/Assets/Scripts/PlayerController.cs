030c3e0fdbcdaf98da5b2eedd9635f5505906425
737104-glitch <737104@student.dusd.net>
2026-04-28 00:28:06 -0700

    Bonus
    finished

--- P4PerezUnit4/Assets/Scripts/PlayerController.cs
+++ P4PerezUnit4/Assets/Scripts/PlayerController.cs
@@ -12,7 +12,19 @@ public class PlayerController : MonoBehaviour
     private float powerupStrength = 15.0f;
 
     public GameObject powerupIndicator;
-    
+
+    public PowerUpType currentPowerUp = PowerUpType.None;
+
+    public GameObject rocketPrefab;
+    private GameObject tmpRocket;
+    private Coroutine powerUpCountdown;
+
+    public float hangTime;
+    public float smashSpeed;
+    public float explosionForce;
+    public float explosionRadius;
+    bool smashing = false;
+    float floorY;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,6 +39,18 @@ public class PlayerController : MonoBehaviour
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
 
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+
+        if (currentPowerUp == PowerUpType.Rockets && Input.GetKeyDown(KeyCode.F))
+        {
+
+        }
+        if (currentPowerUp == PowerUpType.Smash && Input.GetKeyDown(KeyCode.Space) &&
+!smashing)
+        {
+            smashing = true;
+            StartCoroutine(Smash());
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -34,16 +58,19 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            currentPowerUp = other.gameObject.GetComponent<PowerUp>().powerUpType;
             powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
+
+
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-            if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
-            {
+        if (collision.gameObject.CompareTag("Enemy") && currentPowerUp == PowerUpType.Pushback)
+        {
             Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
             Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
 
@@ -51,13 +78,57 @@ public class PlayerController : MonoBehaviour
 
             Debug.Log("Collided with " + collision.gameObject.name + "with powerup set to " + hasPowerup);
             enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            Debug.Log("Player collided with:" + collision.gameObject.name + "with powerup set to " + currentPowerUp.ToString());
+        }
+
+        void LaunchRockets()
+        {
+            foreach (var enemy in FindObjectsOfType<Enemy>())
+            {
+                tmpRocket = Instantiate(rocketPrefab, transform.position + Vector3.up, Quaternion.identity);
+                tmpRocket.GetComponent<RocketBehaviour>().Fire(enemy.transform);
             }
+        }
     }
 
     IEnumerator PowerupCountdownRoutine()
     {
         yield return new WaitForSeconds(7);
         hasPowerup = false;
+        currentPowerUp = PowerUpType.None;
         powerupIndicator.gameObject.SetActive(false);
     }
+
+    IEnumerator Smash()
+    {
+        var enemies = FindObjectsOfType<Enemy>();
+        
+        floorY = transform.position.y;
+
+        float jumpTime = Time.time + hangTime;
+
+        while (Time.time < jumpTime)
+        {
+          
+            playerRb.velocity = new Vector2(playerRb.velocity.x, smashSpeed);
+            yield return null;
+        }
+        
+        while (transform.position.y > floorY)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, -smashSpeed * 2);
+            yield return null;
+        }
+       
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            
+            if (enemies[i] != null)
+                enemies[i].GetComponent<Rigidbody>().AddExplosionForce(explosionForce,
+                transform.position, explosionRadius, 0.0f, ForceMode.Impulse);
+        }
+       
+        smashing = false;
+    }
+
 }
