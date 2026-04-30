9096fa2c28c944bc1adc7a8b98212fb2a08bb5ab
737104-glitch <737104@student.dusd.net>
2026-04-29 21:21:16 -0700

    finished
    finished

--- P4PerezUnit4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ P4PerezUnit4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -14,7 +14,8 @@ public class PlayerControllerX : MonoBehaviour
 
     private float normalStrength = 10; // how hard to hit enemy without powerup
     private float powerupStrength = 25; // how hard to hit enemy with powerup
-    
+
+    private float turboBoost = 10f;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -31,6 +32,11 @@ public class PlayerControllerX : MonoBehaviour
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(focalPoint.transform.forward * turboBoost, ForceMode.Impulse);
+        }
+       
     }
 
     // If Player collides with powerup, activate powerup
@@ -38,6 +44,7 @@ public class PlayerControllerX : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Powerup"))
         {
+            StartCoroutine(PowerupCooldown());
             Destroy(other.gameObject);
             hasPowerup = true;
             powerupIndicator.SetActive(true);
